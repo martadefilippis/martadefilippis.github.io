71c74b12b131098e07f2ea3dd95265fb99e01aca
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-04-02 14:10:59 +0200

    NewSite work in progress

---
+++ newSite/assets/js/theme.js
@@ -0,0 +1,44 @@
+function toggleColorSetting(elem, event_)
+{
+	event_.preventDefault();
+	const namePreference = "dark-preference";
+	const storedPreference = window.localStorage.getItem(namePreference);
+	var S;
+	if (storedPreference == null || storedPreference == "black")
+        {
+		document.documentElement.classList.add("white-color");
+		S = "white";	
+        }
+        else
+        {       
+		document.documentElement.classList.remove("white-color");
+		S = "black";
+        }
+
+	window.localStorage.setItem(namePreference, S);
+
+	event_.stopPropagation();
+}
+
+function setColorSetting(elem)
+{
+	const namePreference = "dark-preference";
+	const storedPreference = window.localStorage.getItem(namePreference);
+	var S;
+	if (storedPreference == null || storedPreference == "black")
+        {
+		S = "black";
+        }
+        else
+        {       
+		document.documentElement.classList.add("white-color");
+		S = "white";	
+
+		window.localStorage.setItem(namePreference, S);
+        }
+}
+
+document.querySelectorAll(".toggle-darkness").forEach(function(node) {
+			setColorSetting(node);
+		}
+		);
